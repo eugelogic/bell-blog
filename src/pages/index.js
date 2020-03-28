3777a4ccdbb15e9159958a2392eb808c7d1925e9
Eugene Molari <eugelogic@gmail.com>
2020-03-28 22:11:32 +0000

    Add Blogs list to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,48 @@
 import React from "react"
+import { graphql } from 'gatsby'
 import Header from './components/header'
 
-const Layout = () => {
+const Layout = ({data}) => {
+    const { edges } = data.allMarkdownRemark
     return (
         <div>
             <Header />
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                fontFamily: 'avenir'
+            }}>
+                {edges.map(edge => {
+                    const { frontmatter } = edge.node
+                    return (
+                        <div
+                            key={frontmatter.path}
+                            style={{ marginBottom: '20px' }}
+                        >
+                            {frontmatter.title}
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
 
 export default Layout
+
+export const query = graphql`
+    query BlogsListQuery {
+        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        date(formatString: "dddd DD MMMM YYYY")
+                        path
+                    }
+                }
+            }
+        }
+    }
+`
