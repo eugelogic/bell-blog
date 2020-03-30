e303bc2941d7b3761c8954522dc93e4829578f10
Eugene Molari <eugelogic@gmail.com>
2020-03-30 12:41:07 +0100

    Fix location of the Components folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from 'gatsby'
-import Header from './components/header'
+import Header from '../components/header'
 
 const Layout = ({data}) => {
     const { edges } = data.allMarkdownRemark
